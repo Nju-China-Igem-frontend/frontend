2718f77492432be2c323a659cc1ca7e8cafb14ad
cuber-straw <2420392704@qq.com>
2020-09-01 15:48:23 +0800

    新增hp模版

--- script/team.js
+++ script/team.js
@@ -3,10 +3,10 @@
 const staggeringOption = {
     delay: 300, // 延迟300ms出现
     distance: "50px", // 从下到上有50像素的移动
-    durationL: 500, // 动画执行500ms
+    durationL: 400, // 动画执行500ms
     easing: "ease-in-out",
     origin: "bottom", // 从下到上
 };
 
-ScrollReveal().reveal(".team-member", {...staggeringOption, interval: 350});
-ScrollReveal().reveal(".supervisor", {...staggeringOption, interval: 350});
+ScrollReveal().reveal(".team-member", {...staggeringOption, interval: 250});
+ScrollReveal().reveal(".supervisor", {...staggeringOption, interval: 250});
