d95559cab4e4687f1dd6071015a22138fa84c950
dongyuanjushi <1978349625@qq.com>
2020-09-01 21:35:52 +0800

    fix: 合并冲突

--- script/Home.js
+++ script/Home.js
@@ -1,3 +1,196 @@
 const Home={
-    template: '<div>Home</div>'
+    template: '<div class="content-wrapper">\n' +
+        '        <!-- 团队成员介绍，15人 -->\n' +
+        '        <section class="team-intro">\n' +
+        '            <h2 class="title">团队成员</h2>\n' +
+        '            <div class="team-members">\n' +
+        '                <div class="team-member">\n' +
+        '                    <div class="profile-image">\n' +
+        '                        <img src="" alt=""/>\n' +
+        '                    </div>\n' +
+        '                    <div class="member-info">\n' +
+        '                        <h4 class="name">小明</h4>\n' +
+        '                        <p class="major">生物制药</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="team-member">\n' +
+        '                    <div class="profile-image">\n' +
+        '                        <img src="" alt=""/>\n' +
+        '                    </div>\n' +
+        '                    <div class="member-info">\n' +
+        '                        <h4 class="name">小明</h4>\n' +
+        '                        <p class="major">生物制药</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="team-member">\n' +
+        '                    <div class="profile-image">\n' +
+        '                        <img src="" alt=""/>\n' +
+        '                    </div>\n' +
+        '                    <div class="member-info">\n' +
+        '                        <h4 class="name">小明</h4>\n' +
+        '                        <p class="major">生物制药</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="team-member">\n' +
+        '                    <div class="profile-image">\n' +
+        '                        <img src="" alt=""/>\n' +
+        '                    </div>\n' +
+        '                    <div class="member-info">\n' +
+        '                        <h4 class="name">小明</h4>\n' +
+        '                        <p class="major">生物制药</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="team-member">\n' +
+        '                    <div class="profile-image">\n' +
+        '                        <img src="" alt=""/>\n' +
+        '                    </div>\n' +
+        '                    <div class="member-info">\n' +
+        '                        <h4 class="name">小明</h4>\n' +
+        '                        <p class="major">生物制药</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="team-member">\n' +
+        '                    <div class="profile-image">\n' +
+        '                        <img src="" alt=""/>\n' +
+        '                    </div>\n' +
+        '                    <div class="member-info">\n' +
+        '                        <h4 class="name">小明</h4>\n' +
+        '                        <p class="major">生物制药</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="team-member">\n' +
+        '                    <div class="profile-image">\n' +
+        '                        <img src="" alt=""/>\n' +
+        '                    </div>\n' +
+        '                    <div class="member-info">\n' +
+        '                        <h4 class="name">小明</h4>\n' +
+        '                        <p class="major">生物制药</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="team-member">\n' +
+        '                    <div class="profile-image">\n' +
+        '                        <img src="" alt=""/>\n' +
+        '                    </div>\n' +
+        '                    <div class="member-info">\n' +
+        '                        <h4 class="name">小明</h4>\n' +
+        '                        <p class="major">生物制药</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="team-member">\n' +
+        '                    <div class="profile-image">\n' +
+        '                        <img src="" alt=""/>\n' +
+        '                    </div>\n' +
+        '                    <div class="member-info">\n' +
+        '                        <h4 class="name">小明</h4>\n' +
+        '                        <p class="major">生物制药</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="team-member">\n' +
+        '                    <div class="profile-image">\n' +
+        '                        <img src="" alt=""/>\n' +
+        '                    </div>\n' +
+        '                    <div class="member-info">\n' +
+        '                        <h4 class="name">小明</h4>\n' +
+        '                        <p class="major">生物制药</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="team-member">\n' +
+        '                    <div class="profile-image">\n' +
+        '                        <img src="" alt=""/>\n' +
+        '                    </div>\n' +
+        '                    <div class="member-info">\n' +
+        '                        <h4 class="name">小明</h4>\n' +
+        '                        <p class="major">生物制药</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="team-member">\n' +
+        '                    <div class="profile-image">\n' +
+        '                        <img src="" alt=""/>\n' +
+        '                    </div>\n' +
+        '                    <div class="member-info">\n' +
+        '                        <h4 class="name">小明</h4>\n' +
+        '                        <p class="major">生物制药</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '            </div>\n' +
+        '        </section>\n' +
+        '        <!-- 指导教师介绍 -->\n' +
+        '        <section class="supervisor-intro">\n' +
+        '            <h2 class="title">导师</h2>\n' +
+        '            <div class="supervisors">\n' +
+        '                <div class="supervisor">\n' +
+        '                    <div class="supervisor-profile">\n' +
+        '                        <img src="" alt="">\n' +
+        '                    </div>\n' +
+        '                    <div class="supervisor-info">\n' +
+        '                        <h4 class="name">小红</h4>\n' +
+        '                        <p class="intro">南京大学校长</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="supervisor">\n' +
+        '                    <div class="supervisor-profile">\n' +
+        '                        <img src="" alt="">\n' +
+        '                    </div>\n' +
+        '                    <div class="supervisor-info">\n' +
+        '                        <h4 class="name">小红</h4>\n' +
+        '                        <p class="intro">南京大学校长</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="supervisor">\n' +
+        '                    <div class="supervisor-profile">\n' +
+        '                        <img src="" alt="">\n' +
+        '                    </div>\n' +
+        '                    <div class="supervisor-info">\n' +
+        '                        <h4 class="name">小红</h4>\n' +
+        '                        <p class="intro">南京大学校长</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="supervisor">\n' +
+        '                    <div class="supervisor-profile">\n' +
+        '                        <img src="" alt="">\n' +
+        '                    </div>\n' +
+        '                    <div class="supervisor-info">\n' +
+        '                        <h4 class="name">小红</h4>\n' +
+        '                        <p class="intro">南京大学校长</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="supervisor">\n' +
+        '                    <div class="supervisor-profile">\n' +
+        '                        <img src="" alt="">\n' +
+        '                    </div>\n' +
+        '                    <div class="supervisor-info">\n' +
+        '                        <h4 class="name">小红</h4>\n' +
+        '                        <p class="intro">南京大学校长</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="supervisor">\n' +
+        '                    <div class="supervisor-profile">\n' +
+        '                        <img src="" alt="">\n' +
+        '                    </div>\n' +
+        '                    <div class="supervisor-info">\n' +
+        '                        <h4 class="name">小红</h4>\n' +
+        '                        <p class="intro">南京大学校长</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="supervisor">\n' +
+        '                    <div class="supervisor-profile">\n' +
+        '                        <img src="" alt="">\n' +
+        '                    </div>\n' +
+        '                    <div class="supervisor-info">\n' +
+        '                        <h4 class="name">小红</h4>\n' +
+        '                        <p class="intro">南京大学校长</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '                <div class="supervisor">\n' +
+        '                    <div class="supervisor-profile">\n' +
+        '                        <img src="" alt="">\n' +
+        '                    </div>\n' +
+        '                    <div class="supervisor-info">\n' +
+        '                        <h4 class="name">小红</h4>\n' +
+        '                        <p class="intro">南京大学校长</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '            </div>\n' +
+        '        </section>\n' +
+        '    </div>'
 }
