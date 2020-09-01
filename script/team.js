b14bb61e678a6e876a00e800b7923d2bb97bd77f
cuber-straw <2420392704@qq.com>
2020-09-01 23:01:03 +0800

    样式美化

--- script/team.js
+++ script/team.js
@@ -1,12 +1,12 @@
 // team 页面js文件
 
 const staggeringOption = {
-    delay: 300, // 延迟300ms出现
+    delay: 200, // 延迟300ms出现
     distance: "50px", // 从下到上有50像素的移动
-    durationL: 400, // 动画执行500ms
+    duration: 400, // 动画执行500ms
     easing: "ease-in-out",
     origin: "bottom", // 从下到上
 };
 
-ScrollReveal().reveal(".team-member", {...staggeringOption, interval: 250});
-ScrollReveal().reveal(".supervisor", {...staggeringOption, interval: 250});
+ScrollReveal().reveal(".team-member", {...staggeringOption, interval: 150});
+ScrollReveal().reveal(".supervisor", {...staggeringOption, interval: 150});
